c9adc3f3beb9e9e1c5d603f2b046ec6cdc59e73c
salamanderman234 <tresnasaputra9@gmail.com>
2022-12-14 20:28:58 +0800

    fix: fixing switch model

--- models/switch.go
+++ models/switch.go
@@ -7,8 +7,18 @@ import (
 )
 
 type Switch struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	Name      string             `bson:"name,omitempty"`
-	Type      string             `bson:"type,omitempty"`
-	CreatedAt time.Time          `bson:"created_at,omitempty"`
+	ID              primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Name            string             `json:"name,omitempty" bson:"name,omitempty"`
+	Manufacturer    string             `json:"manufacturer,omitempty" bson:"manufacturer,omitempty"`
+	Type            string             `json:"type,omitempty" bson:"type,omitempty"`
+	StemType        string             `json:"stem_type,omitempty" bson:"stem_type,omitempty"`
+	HousingColor    []string           `json:"housing_color,omitempty" bson:"housing_color,omitempty"`
+	StemColor       string             `json:"stem_color,omitempty" bson:"stem_color,omitempty"`
+	RgbOption       bool               `json:"rgb_option,omitempty" bson:"rgb_option,omitempty"`
+	ActuationForce  float64            `json:"actuation_force(g),omitempty" bson:"actuation_force(g),omitempty"`
+	BottomOutForce  float64            `json:"bottom_out_force(g),omitempty" bson:"bottom_out_force(g),omitempty"`
+	ActuationLength float64            `json:"actuation_length(mm),omitempty" bson:"actuation_length(mm),omitempty"`
+	TravelLength    float64            `json:"travel_length(mm),omitempty" bson:"travel_length(mm),omitempty"`
+	Picture         string             `json:"picture,omitempty" bson:"picture,omitempty"`
+	CreatedAt       time.Time          `json:"-" bson:"created_at,omitempty"`
 }
