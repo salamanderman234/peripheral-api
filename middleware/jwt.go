24f68f02327596edcaebfc6c602bc9532ccdf194
salamanderman234 <tresnasaputra9@gmail.com>
2022-12-17 18:39:19 +0800

    feat: creating jwt middleware

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -3,12 +3,15 @@ package middleware
 import (
 	"net/http"
 
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
+	"github.com/salamanderman234/peripheral-api/config"
 	"github.com/salamanderman234/peripheral-api/entity"
 )
 
 func Jwt(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
+		var claims entity.JWTClaims
 		token, err := ctx.Cookie("token")
 		if err != nil || token.Value == "" {
 			return ctx.JSON(http.StatusUnauthorized, entity.BaseResponse{
@@ -17,5 +20,18 @@ func Jwt(next echo.HandlerFunc) echo.HandlerFunc {
 				Errors: "Missing cookie token",
 			})
 		}
+
+		_, err = jwt.ParseWithClaims(token.Value, claims, func(t *jwt.Token) (interface{}, error) {
+			return []byte(config.GetAppSecretKey()), nil
+		})
+		if err != nil {
+			return ctx.JSON(http.StatusUnauthorized, entity.BaseResponse{
+				Code:   http.StatusUnauthorized,
+				Status: "Unauthorized",
+				Errors: "Invalid token",
+			})
+		}
+
+		return next(ctx)
 	}
 }
